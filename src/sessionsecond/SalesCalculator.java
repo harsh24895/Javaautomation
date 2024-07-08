47d2b229ebaf44e01a20692dfbc4e3d99e15f184
harsh24895 <40701334+harsh24895@users.noreply.github.com>
2024-07-08 20:40:42 -0300

    sessionsecond

---
+++ src/sessionsecond/SalesCalculator.java
@@ -0,0 +1,22 @@
+package sessionsecond;
+
+import java.util.Scanner;
+
+public class SalesCalculator {
+    public static void main(String[] args){
+
+        int salary = 5000;
+        int bonus= 2000;
+        System.out.println("Enter the number of sales you have done");
+        Scanner sc = new Scanner(System.in);
+        int sales = sc.nextInt();
+        if(sales>50){
+            salary = salary + bonus;
+            System.out.println("You total is"+salary);
+        }else{
+            System.out.println("please  try next time");
+
+        }
+    }
+
+}
