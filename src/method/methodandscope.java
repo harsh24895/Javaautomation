831e3c16b543bc9e51cfbf789243c394cef44e76
harsh24895 <40701334+harsh24895@users.noreply.github.com>
2024-08-11 14:16:27 -0300

    Change

--- src/method/methodandscope.java
+++ src/method/methodandscope.java
@@ -2,6 +2,17 @@ package method;
 
 public class methodandscope {
     public static void main(String[] args) {
+
+        public static void getSalary(){
+            double rest = 522.0;
+
+
+        }
+
+        public static int getCredit(){
+
+        }
         System.out.println("Enter the number");
+
     }
 }
