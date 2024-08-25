036198ee51a06161a31a24f61ca82e687d3d7f00
harsh24895 <40701334+harsh24895@users.noreply.github.com>
2024-08-25 15:40:19 -0300

    Method overloading

---
+++ src/rectangleobject/MethodOverloading.java
@@ -0,0 +1,27 @@
+package rectangleobject;
+
+public class MethodOverloading {
+
+    //Method overloading
+    // Here we are using the same method name but different parameters
+    public static String getMonth(int month){
+        switch(month){
+            case 1:return "Jan";
+            case 2: return "Feb";
+            case 3: return "March";
+            default: return "invalid month. please enter again";
+        }
+
+
+    }
+    public static int getMonth(String month){
+
+        switch (month){
+            case "Jan":return 1;
+            case "Feb":return 2;
+            case "March":return 3;
+            default:return 0;
+        }
+
+    }
+}
---
+++ src/rectangleobject/MonthConverter.java
@@ -0,0 +1,11 @@
+package rectangleobject;
+
+public class MonthConverter {
+    public static void main(String[] args) {
+
+        //here new don't have to instantiate that class in order to access
+        System.out.println(MethodOverloading.getMonth(2));
+        System.out.println(MethodOverloading.getMonth("March"));
+
+    }
+}
