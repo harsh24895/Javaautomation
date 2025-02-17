496bba3971d22d3753be15f5473f3a1d3b2f3bfe
harsh24895 <40701334+harsh24895@users.noreply.github.com>
2025-02-17 17:19:00 -0400

    update file and comments

--- src/rectangleobject_Ch6/HomeCalRedo.java
+++ src/rectangleobject_Ch6/HomeCalRedo.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 
 public class HomeCalRedo {
     public static void main(String[] args) {
+        //Ch_6c
         //Loook for Rectangle class here are redoing this practice on rectangle class
+        //here we are calling the method without static declare as static method does not required any instance
+        // So if we want to call getRoom in main method without declaring the static, we need to intentiaite the Class which we are in currently
         // here we declare new HomeCalRedo as we don't want to create a new instance of class but we still want to access from the class
-        //here we are calling the method without static declare
         HomeCalRedo calculator = new HomeCalRedo(); //also in java the HomeCalRedo is a constructor define implictly
         Rectangle rectkitchen = calculator.getRoom();
         Rectangle bath = calculator.getRoom();
--- src/rectangleobject_Ch6/HomeCalculator.java
+++ src/rectangleobject_Ch6/HomeCalculator.java
@@ -2,10 +2,11 @@ package rectangleobject_Ch6;
 
 public class HomeCalculator {
     public static void main(String[] args) {
+        ///Ch_6b
        //look for Rectangle class is attach to it
         //Find the area for the two rooms
         //Rectange 1
-        // we created a new object and calling the constructor Rectangle()
+        // we created a new object and calling the default constructor Rectangle()
         Rectangle rectangle1 = new Rectangle();
         rectangle1.setWidth(25);
         rectangle1.setLength(80);
@@ -13,7 +14,7 @@ public class HomeCalculator {
         double areaofroom1 = rectangle1.calArea();
 
         //rectangle 2
-        //we are using other constructor
+        //we are using other parameterized constructor
         Rectangle rectangle2 = new Rectangle(50,90);
         double arearoom2 = rectangle2.calArea();
 
